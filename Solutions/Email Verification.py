e9f117e77b574945bccb3098a512406533057804
BehradX <31148015+behradkhadem@users.noreply.github.com>
2019-03-17 16:31:47 +0330

    Update Email Verification.py
    added comments and etc.

--- Solutions/Email Verification.py
+++ Solutions/Email Verification.py
@@ -1,9 +1,11 @@
-import re
-email = input("Please enter your email. \n")
+import re #Regular Expression library for Python.
 
 def checkMail(email):
-    match = re.match('^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,4})$', email, re.IGNORECASE)
+    match = re.match('^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,4})$', email, re.IGNORECASE) #we used regular expression for the hard part!
     if match:
         return True
     else:
         return False
+
+email = input("Please enter your email. \n")
+print(checkMail(email)) #It just returns True and False but you can play with it.
